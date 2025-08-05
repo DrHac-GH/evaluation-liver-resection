5d5f5030301de297f7c4747413b45ba400604920
DrHac-GH <kazuki_h0612@hotmail.com>
2025-08-05 10:36:15 +0900

    fix: Configure basePath and assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/evaluation-liver-resection', // GitHub Pagesのリポジトリ名
+  assetPrefix: '/evaluation-liver-resection/', // GitHub Pagesのリポジトリ名
 };
 
 export default nextConfig;
